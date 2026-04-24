bf39b8f2fe28679d32dcda00b935a34ccd02e610
maggie-davies <md6053@nyu.edu>
2026-04-23 22:25:41 -0400

    Map is working and added formatting

--- scripts.java
+++ scripts.java
@@ -1,1 +1,26 @@
-// creates the map, setting the container to the id of the div I added in the html code, and setting the initial center and zoom level of the m
+    mapboxgl.accessToken = 'pk.eyJ1IjoibWFnZ2llLWRhdmllcyIsImEiOiJjbW9jOG13Z3cwOHhhMnFwem5hdzlpdTQ5In0.mHjj1Cv4VWxIRFTlXrDECA';
+    
+    const map = new mapboxgl.Map({
+      container: 'map-container', // container ID
+      style: 'mapbox://styles/mapbox/standard', // Using the standard Mapbox style
+      config: { 
+        // Mapbox GL JS configuration options
+        // For example, you can set the following options:
+        // - `preserveDrawingBuffer: true` to allow for exporting the map as an image
+        // - `antialias: true` for smoother rendering of 3D objects
+        // - `fadeDuration: 300` to control the fade-in duration of tiles
+        basemap: {
+      showPointOfInterestLabels: false,
+      showRoadLabels: false,
+      showAdminBoundaries: false,
+      showLandmarkIconLabels: false
+        }
+    },
+      },
+      center: [-87.68119, 41.91091], // starting position [long, lat] in the center of my area
+      zoom: 10.04 // starting zoom at a birds-eye city view
+      <!-- Noting other controls for how the map looks on launch 
+                Bearing:
+                Pitch:  -->
+
+    });
