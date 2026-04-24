b1427931ad0aa7d77aea2a34e0c5d01ba61f13ec
maggie-davies <md6053@nyu.edu>
2026-04-24 00:18:10 -0400

    Added a marker for kasama, code is working

--- scripts.java
+++ scripts.java
@@ -3,24 +3,24 @@
     const map = new mapboxgl.Map({
       container: 'map-container', // container ID
       style: 'mapbox://styles/mapbox/standard', // Using the standard Mapbox style
+      center: [-87.68119, 41.91091], // starting position [long, lat] in the center of my area
+      zoom: 10.04, // starting zoom at a birds-eye city view
+      //Noting other controls for how the map looks on launch 
+                //Bearing:
+                //Pitch: 
       config: { 
         // Mapbox GL JS configuration options
-        // For example, you can set the following options:
-        // - `preserveDrawingBuffer: true` to allow for exporting the map as an image
-        // - `antialias: true` for smoother rendering of 3D objects
-        // - `fadeDuration: 300` to control the fade-in duration of tiles
         basemap: {
-      showPointOfInterestLabels: false,
-      showRoadLabels: false,
-      showAdminBoundaries: false,
-      showLandmarkIconLabels: false
-        }
-    },
-      },
-      center: [-87.68119, 41.91091], // starting position [long, lat] in the center of my area
-      zoom: 10.04 // starting zoom at a birds-eye city view
-      <!-- Noting other controls for how the map looks on launch 
-                Bearing:
-                Pitch:  -->
+            showPointOfInterestLabels: false,
+            showRoadLabels: false,
+            showAdminBoundaries: false,
+            showLandmarkIconLabels: false
+            }
+        },
+    }
+    );
 
-    });
+    // Create a default Marker and add it to the map.
+    const marker_kasama = new mapboxgl.Marker()
+        .setLngLat([-87.675578, 41.908166])
+        .addTo(map);
